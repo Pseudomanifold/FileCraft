eed243650c277d3ae95273fc9b8155a3d4654931
Bastian Rieck <bastian@rieck.ru>
2015-05-14 15:22:35 +0200

    Simple intersection tests for bounding boxes

--- BoundingBox.cc
+++ BoundingBox.cc
@@ -14,6 +14,20 @@ BoundingBox::BoundingBox( float xMin, float xMax,
 {
 }
 
+bool BoundingBox::intersects( const BoundingBox& other ) const noexcept
+{
+  if( _xMin > other._xMax || other._xMin > _xMax )
+    return false;
+
+  if( _yMin > other._yMax || other._yMin > _yMax )
+    return false;
+
+  if( _zMin > other._zMax || other._zMin > _zMax )
+    return false;
+
+  return true;
+}
+
 void BoundingBox::checkExtents() const
 {
   bool valid =    _xMin <= _xMax
--- BoundingBox.hh
+++ BoundingBox.hh
@@ -8,6 +8,15 @@ public:
                float yMin, float yMax,
                float zMin, float zMax );
 
+  float xMin() const noexcept { return _xMin; }
+  float xMax() const noexcept { return _xMax; }
+  float yMin() const noexcept { return _yMin; }
+  float yMax() const noexcept { return _yMax; }
+  float zMin() const noexcept { return _zMin; }
+  float zMax() const noexcept { return _zMax; }
+
+  bool intersects( const BoundingBox& other ) const noexcept;
+
 private:
   void checkExtents() const;
 
